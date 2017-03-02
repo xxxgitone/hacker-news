4995371e78979098cc078735608c0cc33d0a44ea
xxxgitone <xxxgit@sina.com>
2017-03-02 21:21:54 +0800

    project init

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nav">
+        <nav>
+            <ul>
+                <li><i>R</i><a href="#">Top</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default new Vue({
+        name: 'home',
+    })
+</script>
+
+<style lang="scss">
+    .nav {
+        width: 30%;
+        height: 100%;
+        background: #ffffff;
+    }
+</style>
