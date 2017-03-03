c0ec32b755cb6cebffd89fafa336ed3af1febd18
xxxgitone <xxxgit@sina.com>
2017-03-03 13:19:47 +0800

    logo

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,75 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="slideBar">
+      <header class="logo">
+        <img src="https://news.ycombinator.com/favicon.ico" alt="Hacker News">
+        <h1>Hacker News</h1>
+      </header>
+      <nav class="nav">
+          <ul>
+              <li><i>R</i><a href="#">NEW</a></li>
+              <li><i>R</i><a href="#">COMMENTS</a></li>
+              <li><i>R</i><a href="#">SHOW</a></li>
+              <li><i>R</i><a href="#">ASK</a></li>
+              <li><i>R</i><a href="#">JOBS</a></li>
+              <li><i>R</i><a href="#">SUBMIT</a></li>
+              <li><i>R</i><a href="#">COMMENTS</a></li>
+              <li><i>R</i><a href="#">COMMENTS</a></li>               
+          </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: 'app'
 }
 </script>
 
-<style>
+<style lang="scss">
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 20px;
-  height: calc(100vh - 40px);
-  width: calc(100vh - 40px);
+  width: 75rem;
+  height: 38rem;
+  border: 1px solid red;
+  font-size: 1rem;
 }
+
+.slideBar {
+    width: 20%;
+    height: 100%;
+    background:white;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+header.logo {
+  width: 100%;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: red;
+
+  img {
+    width: 15%;
+    height: 30%;
+    border-radius: 50%;
+  }
+
+  h1 {
+    width: 30%;
+    width: 65%;
+    font-size: 1.25rem;
+    text-align: center;
+  }
+}
+
+nav.nav {
+
+}
+
+
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../views/Home.vue'
+// import App from '../App'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      component: Home
+      // path: '/',
+      // component: App
     }
   ]
 })
