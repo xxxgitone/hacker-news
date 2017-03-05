ee316932599fd6da5db2c93daecf0cb6d53861e8
xxxgitone <xxxgit@sina.com>
2017-03-05 20:22:07 +0800

    设计路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import App from '../App'
+import News from '../views/News'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      // path: '/',
-      // component: App
+      path: '/',
+      component: News
+    },
+    {
+      path: '/news',
+      component: News
     }
   ]
 })
