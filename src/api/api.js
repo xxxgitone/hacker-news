734ee8e6438e3a651698e786833895e68dfbcfd8
xxxgitone <xxxgit@sina.com>
2017-03-13 21:30:52 +0800

    分离出api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+//  获取单个user
 export function fetchUser (id) {
   return new Promise((resolve, reject) => {
     axios.get(`https://hacker-news.firebaseio.com/v0/user/${id}.json?print=pretty`)
@@ -9,5 +10,17 @@ export function fetchUser (id) {
     .catch((err) => {
       console.log(err)
     })
+  })
+}
+//  获取单个item
+export function fetchItem (id) {
+  return new Promise(function (resolve, reject) {
+    axios.get(` https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   })
 }
