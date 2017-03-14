89f2a8ab1ee51ac94c4474b01563aa5d572a2e68
xxxgitone <xxxgit@sina.com>
2017-03-14 21:03:19 +0800

    编写loading组件

---
+++ src/components/Loading.vue
@@ -0,0 +1,27 @@
+<template>
+    <p class="loading">
+      <img :src="loadingImg">
+    </p>
+</template>
+
+<script>
+    import loadingImg from '../../static/img/loading.png'
+    export default {
+      name: 'loading',
+      data () {
+        return {
+          loadingImg: loadingImg
+        }
+      }
+    }
+</script>
+
+<style lang="scss">
+    .loading {
+      height: 20em;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+</style>
